c9226056e4d1cb6123081382fefb5a66b0dcce85
tomocy <tomocy@gmail.com>
2021-02-21 14:27:46 +0900

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_riverpod_tutorial/app.dart';
+import 'package:flutter_riverpod_tutorial/todo/app.dart';
 
 void main() {
   runApp(ProviderScope(
--- lib/todo/app.dart
+++ lib/todo/app.dart
@@ -2,14 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod_tutorial/todo/TodoList.dart';
 
 class App extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Todo',
       home: const TodoList(),
     );
   }
